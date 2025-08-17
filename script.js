b0fd2397848d55d2405f97edf439c6ac752f7492
ArtemiiHH <artemiishchukin2003@gmail.com>
2025-08-17 18:38:57 +0300

    Add getHumanChoice function

--- script.js
+++ script.js
@@ -6,14 +6,23 @@
 // Check the result of the two picks
 // Claim the winner
 
+const getHumanChoice = () => {
+  const input = parseInt(
+    prompt(
+      "Please pick a number between 1-3. 1 is Rock, 2 is Paper, 3 is Scissors:"
+    )
+  );
+};
+getHumanChoice();
+
 const getComputerChoice = () => {
   const randomNum = Math.floor(Math.random() * 3 + 1);
   if (randomNum === 1) {
-    console.log('Rock');
+    console.log("Rock");
   } else if (randomNum === 2) {
-    console.log('Paper');
+    console.log("Paper");
   } else {
-    console.log('Scissors');
+    console.log("Scissors");
   }
 };
 getComputerChoice();
